3abb886084721e5ae30141df5317e6bc6765d900
eduanlima <eduandev@gmail.com>
2022-01-26 12:29:59 -0300

    Improving verification

--- validate-cpf/src/main/ValidateCPF.java
+++ validate-cpf/src/main/ValidateCPF.java
@@ -18,26 +18,39 @@ public class ValidateCPF {
 		String[] cpfAsArray = cpf.toString().split("");
 		int sum = 0;
 		
-		for (int i = 0; i < 9; i++) {
-			sum = sum + (Integer.parseInt(cpfAsArray[i]) * (10 - i)); 
-		}
-		
-		Integer firstChecker = 11 - (sum % 11);
-		firstChecker = firstChecker == 11 || firstChecker == 10 ? 0 : firstChecker;
-		
-		if (firstChecker.toString().equals(cpfAsArray[9])) {
+		if (cpfAsArray.length == 11) {
 			
-			sum = 0;
-			
-			for (int i = 0; i < 10; i++) {
-				sum = sum + (Integer.parseInt(cpfAsArray[i]) * (11 - i)); 
+			//Checking the first check digit.
+			for (int i = 0; i < 9; i++) {
+				sum = sum + (Integer.parseInt(cpfAsArray[i]) * (10 - i)); 
 			}
 			
-			Integer secondChecker = 11 - (sum % 11);
-			secondChecker = secondChecker == 11 || secondChecker == 10 ? 0 : secondChecker;
+			Integer firstChecker = 11 - (sum % 11);
+			firstChecker = firstChecker == 11 || firstChecker == 10 ? 0 : firstChecker;
 			
-			if (secondChecker.toString().equals(cpfAsArray[10])) {
-				System.out.println("CPF is valid.");
+			if (firstChecker.toString().equals(cpfAsArray[9])) {
+				
+				//Checking the second check digit and if there is at least one digit different from others.
+				int checkNumbers = 0;
+				sum = 0;
+				
+				for (int i = 0; i < 10; i++) {
+					sum = sum + (Integer.parseInt(cpfAsArray[i]) * (11 - i));
+					
+					if (cpfAsArray[i].equals(cpfAsArray[i + 1])) {
+						checkNumbers++;
+					}
+				}
+				
+				Integer secondChecker = 11 - (sum % 11);
+				secondChecker = secondChecker == 11 || secondChecker == 10 ? 0 : secondChecker;
+				
+				if (secondChecker.toString().equals(cpfAsArray[10]) && checkNumbers != 10) {
+					System.out.println("CPF is valid.");
+				}
+				else {
+					System.out.println("CPF is not valid.");
+				}
 			}
 			else {
 				System.out.println("CPF is not valid.");
@@ -49,4 +62,4 @@ public class ValidateCPF {
 	}
 }
 
-//cpf test 53421773750 81763554880 23813481077 91983221503 48010394483 18342178730
+//CPF test: 53421773750 81763554880 23813481077 91983221503 48010394483 18342178730
